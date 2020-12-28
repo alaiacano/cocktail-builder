f886d3d1240fb59f71075c3903d2be5741070d13
Adam Laiacano <adam.laiacano@gmail.com>
2020-12-27 20:53:11 -0500

    fix call to airtable

--- functions/airtable.js
+++ functions/airtable.js
@@ -4,72 +4,12 @@ const Airtable = require("airtable");
 /** THIS IS YOUR SERVERLESS FUNCTION */
 exports.handler = async (event, context, callback) => {
   //pull the required information from your environment variables, which can be set in the Netlify UI
-  // const {
-  //   AIRTABLE_API_URL,
-  //   AIRTABLE_API_CLIENT_ID,
-  //   AIRTABLE_API_KEY,
-  // } = process.env;
-  return {
-    statusCode: 200,
-    body: JSON.stringify([
-      {
-        bitters: ["Angostura Bitters"],
-        liqueurs: ["Sweet Vermouth"],
-        spirits: ["Rye"],
-        name: "Manhattan",
-      },
-      {
-        sweeteners: ["Simple Syrup"],
-        juices: ["Lime"],
-        liqueurs: ["Cointreau"],
-        spirits: ["Blanco Tequila"],
-        name: "Margarita",
-      },
-      {
-        juices: ["Lemon"],
-        book: "The Office",
-        liqueurs: ["Creme de Violette", "Maraschino"],
-        spirits: ["Gin"],
-        name: "Aviation",
-      },
-      {
-        other: ["Heavy Cream"],
-        bitters: ["Angostura Bitters", "Orange Bitters"],
-        book: "The Office",
-        liqueurs: ["Creme de Cacao"],
-        spirits: ["Cognac"],
-        name: "Brandy Alexander",
-      },
-      {
-        sweeteners: ["Simple Syrup"],
-        other: [
-          "Cinnamon Sticks",
-          "Star Anise",
-          "Vanilla Bean",
-          "Nutmeg",
-          "Rosemary",
-        ],
-        juices: ["Lemon", "Apple Cider", "Cranberry Juice Cocktail"],
-        book: "The Aviary Winter Cocktails",
-        spirits: ["Reposado Tequila"],
-        name: "Cider Margarita",
-      },
-      {
-        other: ["Lime", "Tonic"],
-        spirits: ["Gin"],
-        name: "Gin & Tonic",
-      },
-      {
-        name: "Cherry Grapefruit Gin & Tonic",
-        book: "The Aviary Summer Cocktails",
-        other: ["Roasted Red Peppers", "Strawberries", "Ancho Chile Powder"],
-        spirits: ["Gin"],
-        juices: ["Lemon", "Lime"],
-        liqueurs: ["Maraschino"],
-        bitters: ["Celery Bitters"],
-      },
-    ]),
-  };
+  const {
+    AIRTABLE_API_URL,
+    AIRTABLE_API_CLIENT_ID,
+    AIRTABLE_API_KEY,
+  } = process.env;
+
   // CONFIGURE YOUR AIRTABLE BASE CONNECTION
   Airtable.configure({
     endpointUrl: AIRTABLE_API_URL,
